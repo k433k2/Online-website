7416fd078117cc2c29ab0d3a66a9e4d0c0953592
k433k2 <162943304+k433k2@users.noreply.github.com>
2025-07-07 21:41:32 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const path = require('path');
+const fs = require('fs');
 
+// Define the schema for storing processed file information
 const FileSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -37,7 +40,24 @@ const FileSchema = new mongoose.Schema({
     }
 });
 
-// Index for automatic expiration
+// TTL index to automatically delete expired file documents
 FileSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+// Optional: virtual property to generate a download URL
+FileSchema.virtual('downloadUrl').get(function () {
+    return `/api/files/${this._id}`;
+});
+
+// Optional: Pre-remove hook to delete physical file from disk (not required unless managing files actively)
+FileSchema.pre('remove', function (next) {
+    if (fs.existsSync(this.path)) {
+        fs.unlink(this.path, (err) => {
+            if (err) console.error('Error deleting file from disk:', err);
+            next();
+        });
+    } else {
+        next();
+    }
+});
 
 module.exports = mongoose.model('File', FileSchema);
